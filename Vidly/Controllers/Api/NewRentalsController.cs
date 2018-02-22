8bf411fedea948a9a3e9ee03d76fdfb017efd07d
JoshuaASelkirk <35569800+boatlife85@users.noreply.github.com>
2018-02-22 08:55:04 -0500

    Building the Simplest API

---
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Dtos;
+
+namespace Vidly.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            var customer = _context.Customers.Single(c => c.Id == newRental.CustomerId);
+            throw new NotImplementedException();
+        }
+    }
+}
