2645865f46e054cea1783f761f6637602877ebcd
JoshuaASelkirk <35569800+boatlife85@users.noreply.github.com>
2018-02-22 11:55:35 -0500

    Implementing Client-Side-Validation Successfully (working!!!)

--- Vidly/Controllers/Api/NewRentalsController.cs
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -5,16 +5,56 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Vidly.Dtos;
+using Vidly.Models;
 
 namespace Vidly.Controllers.Api
 {
     public class NewRentalsController : ApiController
     {
+        private ApplicationDbContext _context;
+
+        public NewRentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         [HttpPost]
         public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
         {
+            // Removed but could be usefull
+            //if (newRental.MovieIds.Count == 0) 
+            //    return BadRequest("No Movie Ids have been given.");
+            
             var customer = _context.Customers.Single(c => c.Id == newRental.CustomerId);
-            throw new NotImplementedException();
+
+            // Removed but could be usefull This is used when using SingleOrDefault.
+            //if (customer == null)
+            //    return BadRequest("CustomerId is not valid.");
+
+            var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id)).ToList();
+
+            //if (movies.Count != newRental.MovieIds.Count)
+            //    return BadRequest("One or more MovieIds are invalid.");
+
+            foreach (var movie in movies)
+            {
+                if (movie.NumberAvailable == 0)
+                    return BadRequest("Movie is not available.");
+
+                movie.NumberAvailable--;
+
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now
+                };
+
+                _context.Rentals.Add(rental);
+            }
+            _context.SaveChanges();
+
+            return Ok();
         }
     }
 }
